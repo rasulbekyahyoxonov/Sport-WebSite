8ccb51405ab1d7516a40b8e30bbef5a0d96b859a
rasulbekyahyoxonov <rasulbekyahyoxonov2@gmail.com>
2022-05-04 10:32:33 +0300

    web site end

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const loader = document.getElementById('spinner_wrapper');
+    setTimeout(() => {
+        loader.style.display = 'none';
+    }, 2000)
+});
+// Loader
+
 window.addEventListener ("scroll", function(){
     let topnawbar = document.querySelector(".nawbar");
     topnawbar.classList.toggle("sticky", window.scrollY > 150);
@@ -72,3 +80,166 @@ counters.forEach((counter) => {
     };
     updateCounter();
 });
+
+// gridDiv
+
+window.addEventListener('DOMContentLoaded', () => {
+    class Grid {
+        constructor(src, alt, num, parentSelector, ...classes)
+        {
+            this.src = src;
+            this.alt = alt
+            this.num = num;
+            this.classes = classes;
+            this.parent = document.querySelector(parentSelector)
+        }
+        render ()
+        {
+            const element = document.createElement('div');
+    
+            if(this.classes.length === 0)
+            {
+                this.classes = 'colGrid'
+                element.classList.add(this.classes)
+            }
+            else
+            {
+                this.classes.forEach(className => element.classList.add(className))
+            }
+    
+            element.innerHTML = `
+                <div class="colGrid">
+                    <img src="${this.src}" alt="${this.alt}">
+                    <div class="background_grid">
+                        <span class="spnGr">
+                            <i class="fa-solid fa-heart"> ${this.num}</i>
+                        </span>
+                    </div>
+                </div>
+            `
+            this.parent.append(element)
+        }
+    }
+
+    new Grid
+    (
+        'pictures/gallery1.jpg',
+        'none',
+        '10',
+        '.gridSection .gridDiv'
+    ).render()
+
+    new Grid
+    (
+        'pictures/gallery2.jpg',
+        'none',
+        '8',
+        '.gridSection .gridDiv'
+    ).render()
+
+    new Grid
+    (
+        'pictures/gallery3.jpg',
+        'none',
+        '13',
+        '.gridSection .gridDiv'
+    ).render()
+
+    new Grid
+    (
+        'pictures/gallery4.jpg',
+        'none',
+        '7',
+        '.gridSection .gridDiv'
+    ).render()
+
+    new Grid
+    (
+        'pictures/gallery5.jpg',
+        'none',
+        '16',
+        '.gridSection .gridDiv'
+    ).render()
+
+    new Grid
+    (
+        'pictures/gallery6.jpg',
+        'none',
+        '22',
+        '.gridSection .gridDiv'
+    ).render()
+
+    new Grid
+    (
+        'pictures/gallery7.jpg',
+        'none',
+        '4',
+        '.gridSection .gridDiv'
+    ).render()
+
+    new Grid
+    (
+        'pictures/gallery8.jpg',
+        'none',
+        '54',
+        '.gridSection .gridDiv'
+    ).render()
+
+    new Grid
+    (
+        'pictures/gallery9.jpg',
+        'none',
+        '19',
+        '.gridSection .gridDiv'
+    ).render()
+});
+
+// Submit
+
+function mySubmit ()
+{
+    document.getElementById('jsDiv').style.display = 'block';
+    let name = document.getElementById('in1').value;
+    let surname = document.getElementById('in2').value;
+    let message = document.getElementById('area').value;
+    if(name != '')
+    {
+        document.getElementById('js_p').innerHTML = "Familya va xabar kiriting!";
+        document.getElementById('in1').style.borderBlockEnd = '1px solid green';
+        if(surname != '')
+        {
+            document.getElementById('in2').style.borderBlockEnd = '1px solid green';
+            document.getElementById('js_p').innerHTML = "Xabar kiriting!";
+            if(message != '')
+            {
+                document.getElementById('area').style.borderBlockEnd = '1px solid green';
+                document.getElementById('jsDiv').style.background = 'green';
+                document.getElementById('js_p').innerHTML = "Xabar yuborildi." + `<i class="fa-regular fa-check"></i>`;
+                document.getElementById('in1').value = '';
+                document.getElementById('in2').value = '';
+                document.getElementById('area').value = '';
+
+                setTimeout(() => {
+                    document.getElementById('jsDiv').style.display = 'none';
+                }, 2000);
+            }
+            else
+            {
+                document.getElementById('area').style.borderBlockEnd = '1px solid red';
+            }
+        }
+        else
+        {
+            document.getElementById('in2').style.borderBlockEnd = '1px solid red';
+            document.getElementById('area').style.borderBlockEnd = '1px solid red';
+        }
+    }
+    else
+    {
+        document.getElementById('js_p').innerHTML = "Ismingiz, Familyangiz va xabar kiriting!";
+        document.getElementById('jsDiv').style.background = 'rgb(226, 0, 0)';
+        document.getElementById('in1').style.borderBlockEnd = '1px solid red';
+        document.getElementById('in2').style.borderBlockEnd = '1px solid red';
+        document.getElementById('area').style.borderBlockEnd = '1px solid red';
+    }
+}
